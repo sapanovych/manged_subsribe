cc9b8deaeb596c97f5d917e16d2b0009b80f7e49
Stanislav Apanovych <sapanovych@griddynamics.com>
2023-07-19 16:08:15 +0300

    added mangment_policy mode

--- modules/mangment_groups/output.tf
+++ modules/mangment_groups/output.tf
@@ -1,3 +1,4 @@
 output "mengment_groups_id" {
   value = azurerm_management_group.mgmt_group.id
+  description = "Managment group id"
 }
--- modules/mangment_groups_parrent/vars.tf
+++ modules/mangment_groups_parrent/vars.tf
@@ -1,11 +1,14 @@
 variable "mangment_group"{
   type = string
+  description = "Name for management group, also display name too"
 }
 variable "subscription_list" {
   type = list(string)
+  description = "List subscrtions - only names. UUID generated automatically."
 }
 
 variable "parrent_name" {
   type = string
   default = "nonid"
+  description = "Name of the parent management group"
 }
